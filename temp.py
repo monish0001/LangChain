f56531d91deef0dc1e140de8f469ac0a1f36e6cc
Mohd Monish <32845817+monish0001@users.noreply.github.com>
2025-10-13 23:09:13 +0530

    Add example function to perform addition and usage examples

---
+++ temp.py
@@ -0,0 +1,9 @@
+# Example function to add two numbers
+def add(a, b):
+    return a + b
+
+# Example usage
+if __name__ == '__main__':
+    print("Addition of 2 and 3:", add(2, 3))
+    print("Addition of -1 and 1:", add(-1, 1))
+    print("Addition of 0 and 0:", add(0, 0))
